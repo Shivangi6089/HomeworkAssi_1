33a4781a8ba4229a549f7b22e990e229b0121573
Shivangi <sviradiya@njcu.edu>
2020-05-27 16:03:55 +0000

    changed Hello.java to show that I love linux

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
     public static void main(String[] args){ 
 
-	    System.out.println("My favorite programming language is C++");
+	    System.out.println("I am taking CS407 because I like Linux");
               }
 
 }
