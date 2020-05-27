316cf88d75e4cab924e6455100fb5df072d0e178
Shivangi <sviradiya@njcu.edu>
2020-05-27 16:08:11 +0000

    Last commit and Now it says that I like manchuriyan

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
     public static void main(String[] args){ 
 
-	    System.out.println("I am taking CS407 because I like Linux");
+	    System.out.println("My favorite food is manchuriyan");
               }
 
 }
